5abafde0febd469d1889899d89de25b46cc8ca79
ntttoon <ntttoon@gmail.com>
2015-12-29 01:31:49 +0700

    unitinfo big art update

--- js/libs.js
+++ js/libs.js
@@ -25,3 +25,26 @@ function nameColor(element) {
 				break;
 		}
 }
+function nameElement(element) {
+		switch(element)
+		{
+			case "Fire":
+				return "Element_Fire.png";
+				break;
+			case "Water":
+				return "Element_Water.png";
+				break;
+			case "Thunder":
+				return "Element_Thunder.png";
+				break;
+			case "Earth":
+				return "Element_Earth.png";
+				break;
+			case "Dark":
+				return "Element_Dark.png";
+				break;
+			case "Light":
+				return "Element_Light.png";
+				break;
+		}
+}
