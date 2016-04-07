14c74cc2e8354103c16399d821066c3d36086e70
ntttoon <ntttoon@gmail.com>
2016-04-08 00:26:46 +0700

    banner example

---
+++ animagic/js/ntttoon.js
@@ -0,0 +1,23 @@
+// by ntttoon 2016 04/07/2016
+function effect(delay_start,div,effect_in,effect_out,duration,effect_mid,delay_mid,loop_mid){
+	setTimeout(function(){
+		$(div).removeAttr('class');
+		$(div).css('visibility','visible');
+		$(div).addClass('animated ' + effect_in);
+		setTimeout(function(){
+			$(div).removeAttr('class');
+			$(div).addClass('animated ' + effect_mid);
+			$(div).css('animation-iteration-count',loop_mid);
+		}, delay_mid);
+		setTimeout(function(){
+			$(div).removeAttr('class');
+			$(div).css('animation-iteration-count','1');
+			$(div).addClass('animated ' + effect_out);
+			//$(div).on("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd",function(e){
+				//$(this).removeAttr('class');
+				//$(this).removeAttr('style');
+				//$(this).off(e);
+			 //});
+		}, duration);
+	}, delay_start);
+}
