3fb9b21d8edbdbe0268fe2475fb8a51b3400b05f
ntttoon <ntttoon@gmail.com>
2017-06-05 10:39:13 +0700

    update bm

--- bannermaker/js/bm.js
+++ bannermaker/js/bm.js
@@ -20,35 +20,27 @@ function resizeCanvas(){
 }
 
 // Function Rectange
-function addImage(path,img,imgGroup,imgObj,imgDestroyBtn,imgDestroyId,zindex,currBtnId,thumb){
+function addImage(path,img,imgObj,imgDestroyBtn,imgDestroyId,zindex,currBtnId,thumb){
 	//var d = new Date();
 	//var n = d.getTime();
-	var options = document.getElementsByName("alpha");
+	/*var options = document.getElementsByName("alpha");
 	if (options) {
 		for (var i = 0; i < options.length; i++) {
 			if (options[i].checked){
 				 var alpha = options[i].value;
 			}
 		}
-	}
-	
+	}*/
 	// create image layer
 	var img = new Konva.Image({
 		x: 0,
 		y: 0,
-		opacity: alpha
+		draggable: true,
 	});
-	// create group
-	var imgGroup = new Konva.Group({
-        x: 0,
-        y: 0,
-        draggable: true,
-    });
-    layer.add(imgGroup);
-    imgGroup.add(img);
+    layer.add(img);
 	resizeCanvas();
 	//
-	//imgGroup.on("dblclick dbltap", function() {
+	//img.on("dblclick dbltap", function() {
 		//this.destroy();
         //layer.draw();
     //});
@@ -57,22 +49,29 @@ function addImage(path,img,imgGroup,imgObj,imgDestroyBtn,imgDestroyId,zindex,cur
 	var imgObj = new Image();
     imgObj.onload = function() {
         img.image(imgObj);
+		img.setZIndex(zindex - 1);
         layer.draw();
     };
 	imgObj.src = path;
-	// add Anchor
-	addAnchor(imgGroup, 0, 0, 'topLeft');
-    addAnchor(imgGroup, imgObj.width, 0, 'topRight');
-    addAnchor(imgGroup, imgObj.height, imgObj.width, 'bottomRight');
-    addAnchor(imgGroup, 0, imgObj.width, 'bottomLeft');
-	imgGroup.setZIndex(zindex - 1);
-	layer.draw();
+	//
+	var slider = document.getElementById('slider1_opacity'); 
+      slider.onchange = function() {
+        img.opacity(slider.value);
+        layer.batchDraw();    
+      };
+	
+	var slider2 = document.getElementById('slider1_scale'); 
+      slider2.onchange = function() {
+        img.scaleX(slider2.value);
+		img.scaleY(slider2.value);
+        layer.batchDraw();    
+      };
 	// add layer
 	document.getElementById(imgDestroyBtn).innerHTML = '<button id="' + imgDestroyId + '" class="w3-bar-item w3-button"><i class="fa fa-close"></i></button>';
 	document.getElementById(thumb).innerHTML = '<img src="' + imgObj.src + '">';
 	document.getElementById(currBtnId).disabled = true;
 	document.getElementById(imgDestroyId).addEventListener('click', function() {
-		imgGroup.destroy();
+		img.destroy();
 		layer.draw();
 		document.getElementById(currBtnId).disabled = false;
 		document.getElementById(imgDestroyBtn).innerHTML = "";
@@ -136,108 +135,3 @@ function addText(id,currBtnId,destroyBtnId,txtDestroyId,color,textHolder){
     }, false);
 	document.getElementById(textHolder).innerHTML = txt ;
 }
-
-function hideAnchor(){
-	var a = layer.find('.topLeft');
-	var b = layer.find('.topRight');
-	var c = layer.find('.bottomRight');
-	var d = layer.find('.bottomLeft');
-	a.hide();
-	b.hide();
-	c.hide();
-	d.hide();
-	layer.draw();
-}
-
-function showAnchor(){
-	var a = layer.find('.topLeft');
-	var b = layer.find('.topRight');
-	var c = layer.find('.bottomRight');
-	var d = layer.find('.bottomLeft');
-	a.show();
-	b.show();
-	c.show();
-	d.show();
-	layer.draw();
-}
-
-///// Add anchor function /////
-function update(activeAnchor) {
-	var group = activeAnchor.getParent();
-	var topLeft = group.get('.topLeft')[0];
-	var topRight = group.get('.topRight')[0];
-	var bottomRight = group.get('.bottomRight')[0];
-	var bottomLeft = group.get('.bottomLeft')[0];
-	var image = group.get('Image')[0];
-	var anchorX = activeAnchor.getX();
-	var anchorY = activeAnchor.getY();
-	// update anchor positions
-	switch (activeAnchor.getName()) {
-		case 'topLeft':
-			topRight.setY(anchorY);
-			bottomLeft.setX(anchorX);
-			break;
-		case 'topRight':
-			topLeft.setY(anchorY);
-			bottomRight.setX(anchorX);
-			break;
-		case 'bottomRight':
-			bottomLeft.setY(anchorY);
-			topRight.setX(anchorX);
-			break;
-		case 'bottomLeft':
-			bottomRight.setY(anchorY);
-			topLeft.setX(anchorX);
-			break;
-	}
-	image.position(topLeft.position());
-	var width = topRight.getX() - topLeft.getX();
-	var height = bottomLeft.getY() - topLeft.getY();
-	if(width && height) {
-		image.width(width);
-		image.height(height);
-	}
-}
-	
-function addAnchor(group, x, y, name) {
-	var stage = group.getStage();
-	var layer = group.getLayer();
-	var anchor = new Konva.Circle({
-		x: x,
-		y: y,
-		stroke: '#666',
-		fill: '#ddd',
-		strokeWidth: 2,
-		radius: 8,
-		name: name,
-		draggable: true,
-		dragOnTop: false
-	});
-	anchor.on('dragmove', function() {
-		update(this);
-		layer.draw();
-	});
-	anchor.on('mousedown touchstart', function() {
-		group.setDraggable(false);
-		this.moveToTop();
-	});
-	anchor.on('dragend', function() {
-		group.setDraggable(true);
-		layer.draw();
-	});
-	// add hover styling
-	anchor.on('mouseover', function() {
-		var layer = this.getLayer();
-		document.body.style.cursor = 'pointer';
-		this.setStrokeWidth(4);
-		layer.draw();
-	});
-	anchor.on('mouseout', function() {
-		var layer = this.getLayer();
-		document.body.style.cursor = 'default';
-		this.setStrokeWidth(2);
-		layer.draw();
-	});
-	group.add(anchor);
-}
-///// end of Add anchor function /////
