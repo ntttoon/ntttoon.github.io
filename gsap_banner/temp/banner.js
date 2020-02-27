dcf5a333002130342c79d97345376b3896c71cfc
ntttoon <ntttoon@gmail.com>
2020-02-28 00:48:40 +0700

    update tool

--- gsap_banner/temp/banner.js
+++ gsap_banner/temp/banner.js
@@ -1,3 +1,6 @@
+var banner_width = document.getElementById("banner_width").value;
+var banner_height = document.getElementById("banner_height").value;
+
 var sprite = document.getElementById("sprite_holder").textContent;
 
 var css_import = document.getElementById("sprite_css").value;
@@ -10,11 +13,13 @@ var gsap_int = document.getElementById("gsap_int_holder").innerHTML;
 
 var gsapscript = document.getElementById("gsap_script").value;
 
-var html1 = '<!doctype html><html><head><meta charset="utf-8"><title>banner HTML5</title><script src="https://s0.2mdn.net/ads/studio/cached_libs/tweenmax_2.1.2_min.js"></script><script src="https://code.jquery.com/jquery-1.12.0.min.js"></script><style>body{margin:0;padding:0;background-color:#FFF;}#banner{width:336px;height:280px;overflow:hidden;	position:relative;background-color:#FFF;}#banner .sprite{position:absolute;}.sprite {background-image: url('+ sprite +');background-repeat: no-repeat;display: block;}' + css_import + '</style>' + '<style>' + sprite_cssposs + '</style>' + '</head><body><div id="banner">' + sprite_itag + '</div></body>';
+var html1 = '<!doctype html><html><head><meta charset="utf-8"><title>banner HTML5</title><script src="https://s0.2mdn.net/ads/studio/cached_libs/tweenmax_2.1.2_min.js"></script><script src="https://code.jquery.com/jquery-1.12.0.min.js"></script><style>body{margin:0;padding:0;background-color:#FFF;}#banner{width:' + banner_width + 'px;height:' + banner_height + 'px;overflow:hidden;	position:relative;background-color:#FFF;}#banner .sprite{position:absolute;}.sprite {background-image: url('+ sprite +');background-repeat: no-repeat;display: block;}' + css_import + '</style>' + '<style>' + sprite_cssposs + '</style>' + '</head><body><div id="banner">' + sprite_itag + '</div></body>';
 
-var html2 = '<script type="text/javascript">' + gsap_int + 'var tl = new TimelineMax({onComplete: showReplay})' + gsapscript + 'function showReplay(){var loop = setTimeout(tlPlay, 5000);}function tlPlay(){tl.play(0)}' + '</script></html>';
+var alt_html1 = '<!doctype html><html><head><meta charset="utf-8"><title>banner HTML5</title><script src="https://s0.2mdn.net/ads/studio/cached_libs/tweenmax_2.1.2_min.js"></script><script src="https://code.jquery.com/jquery-1.12.0.min.js"></script><style>body{margin:0;padding:0;background-color:#FFF;}#banner{width:' + banner_width + 'px;height:' + banner_height + 'px;overflow:hidden;	position:relative;background-color:#FFF;}#banner .sprite{position:absolute;}.sprite {background-image: url(spritesheet.png);background-repeat: no-repeat;display: block;}' + css_import + '</style>' + '<style>' + sprite_cssposs + '</style>' + '</head><body><div id="banner">' + sprite_itag + '</div></body>';
+
+var html2 = '<script type="text/javascript">' + gsap_int + 'var tl = new TimelineMax({onComplete: showReplay})' + gsapscript + 'function showReplay(){var loop = setTimeout(tlPlay, 5000);}function tlPlay(){tl.play(0);}' + '</script></html>';
 
 var html = html1 + html2;
+var alt_html = alt_html1 + html2;
 var result = html;
-
-//var result2 = tidy_html5(alt_html, options);
+var result2 = alt_html;
