2326a124883e552c9edc1f2641ddaa9c333e9864
kutoon <ntttoon@gmail.com>
2020-12-27 23:40:54 +0700

    update gsap tool

--- gsap_banner/temp/banner.js
+++ gsap_banner/temp/banner.js
@@ -13,8 +13,8 @@ var gsap_int = document.getElementById("gsap_int_holder").innerHTML;
 
 var gsapscript = document.getElementById("gsap_script").value;
 
-var html1 = '<!doctype html><html><head><meta charset="utf-8"><title>banner HTML5</title><script src="https://s0.2mdn.net/ads/studio/cached_libs/tweenmax_2.1.2_min.js"></script><script src="https://code.jquery.com/jquery-1.12.0.min.js"></script><style>body{margin:0;padding:0;background-color:#FFF;}#banner{width:' + banner_width + 'px;height:' + banner_height + 'px;overflow:hidden;	position:relative;background-color:#FFF;}#banner .sprite{position:absolute;}.sprite {background-image: url('+ sprite +');background-repeat: no-repeat;display: block;}' + css_import + sprite_cssposs + '</style>' + '</head><body><div id="banner">' + sprite_itag + '</div></body>';
+var html1 = '<!doctype html><html><head><meta charset="utf-8"><title>banner HTML5</title><script src="https://s0.2mdn.net/ads/studio/cached_libs/gsap_3.5.1_min.js"></script><script src="https://code.jquery.com/jquery-1.12.0.min.js"></script><style>body{margin:0;padding:0;background-color:#FFF;}#banner{width:' + banner_width + 'px;height:' + banner_height + 'px;overflow:hidden;	position:relative;background-color:#FFF;}#banner .sprite{position:absolute;}.sprite {background-image: url('+ sprite +');background-repeat: no-repeat;display: block;}' + css_import + sprite_cssposs + '</style>' + '</head><body><div id="banner">' + sprite_itag + '</div></body>';
 
-var html2 = '<script type="text/javascript">' + gsap_int + 'var tl = new TimelineMax({onComplete: showReplay})' + gsapscript + 'function showReplay(){var loop = setTimeout(tlPlay, 5000);}function tlPlay(){tl.play(0);}' + '</script></html>';
+var html2 = '<script type="text/javascript">' + gsap_int + 'var tl = gsap.timeline({onComplete: showReplay})' + gsapscript + 'function showReplay(){var loop = setTimeout(tlPlay, 5000);}function tlPlay(){tl.play(0);}' + '</script></html>';
 
 var preview_html = html1 + html2;
